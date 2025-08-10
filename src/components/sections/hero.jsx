3216bde6a1fe45347d6073e20d9e514b7c229fec
arifuxd <arifofficial00@gmail.com>
2025-08-10 19:46:31 +0600

    added portfolio link on hero section and client logo

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,62 +1,142 @@
-'use client'
-import React from 'react'
-import Link from 'next/link';
-import { RiFacebookCircleFill, RiTwitterXLine, RiLinkedinFill, RiGithubLine, RiCircleFill, RiDownloadLine } from '@remixicon/react'
-import PartnersMarquee from './partnersMarquee';
-import SlideUp from '@/utlits/animations/slideUp';
+"use client";
+import React from "react";
+import Link from "next/link";
+import {
+  RiFacebookCircleFill,
+  RiTwitterXLine,
+  RiLinkedinFill,
+  RiGithubLine,
+  RiCircleFill,
+  RiDownloadLine,
+  RiBehanceFill,
+  RiPlayCircleFill,
+} from "@remixicon/react";
+import PartnersMarquee from "./partnersMarquee";
+import SlideUp from "@/utlits/animations/slideUp";
 
 const Hero = () => {
-    return (
-        <section id="about" className="about-area">
-            <div className="container">
-                <div className="row">
-                    {/* <!-- START ABOUT IMAGE DESIGN AREA --> */}
-                    <div className="col-lg-4">
-                        <SlideUp>
-                            <div className="about-image-part">
-                                <img src={"/images/about/profile.png"} alt="About Me" />
-                                <h2>Ariful Islam</h2>
-                                <p>Creative Visualizer</p>
-                                <div className="about-social text-center">
-                                    <ul>
-                                        <li><Link href=""><RiFacebookCircleFill size={20} /></Link></li>
-                                        <li><Link href=""><RiTwitterXLine size={20} /></Link></li>
-                                        <li><Link href=""><RiLinkedinFill size={20} /></Link></li>
-                                        <li><Link href=""><RiGithubLine size={20} /></Link></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </SlideUp>
-                    </div>
-                    {/* <!-- / END ABOUT IMAGE DESIGN AREA -->
+  return (
+    <section id="about" className="about-area">
+      <div className="container">
+        <div className="row">
+          {/* <!-- START ABOUT IMAGE DESIGN AREA --> */}
+          <div className="col-lg-4">
+            <SlideUp>
+              <div className="about-image-part">
+                <img src={"/images/about/profile.png"} alt="About Me" />
+                <h2>Ariful Islam</h2>
+                <p>Creative Visualizer</p>
+                <div className="about-social text-center">
+                  <ul>
+                    <li>
+                      <Link
+                        href="https://facebook.com/arifuxd"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <RiFacebookCircleFill size={20} />
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="https://linkedin.com/in/arifuxd"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <RiLinkedinFill size={20} />
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="https://x.com/arifuxd"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <RiTwitterXLine size={20} />
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="https://github.com/arifuxd"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <RiGithubLine size={20} />
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="about-hero-portfolio text-center">
+                  <ul>
+                    <li>
+                      <Link
+                        href="https://behance.net/arifuxd"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <RiBehanceFill size={20} />
+                        <span>behance.net/arifuxd</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="https://cutt.ly/arifuxd"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <RiPlayCircleFill size={20} />
+                        <span>2D & 3D Motion Graphics</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </SlideUp>
+          </div>
+          {/* <!-- / END ABOUT IMAGE DESIGN AREA -->
                     <!-- START ABOUT TEXT DESIGN AREA --> */}
-                    <div className="col-lg-8">
-                        <SlideUp>
-                            <div className="about-content-part">
-                                <p>Hello There!</p>
-                                <h2>
-                                   I’m Ariful Islam, a creative visualizer with 6+ years of experience crafting impactful visuals and multimedia stories.
-
-                                </h2>
-                                <div className="adress-field">
-                                    <ul>
-                                        <li className='d-flex align-items-center'><i><RiCircleFill size={14} /></i> Available for Collaboration</li>
-                                    </ul>
-                                </div>
+          <div className="col-lg-8">
+            <SlideUp>
+              <div className="about-content-part">
+                <p>Hello There!</p>
+                <h2>
+                  I’m Ariful Islam, a creative visualizer with 6+ years of
+                  experience crafting impactful visuals and multimedia stories.
+                </h2>
+                <div className="adress-field">
+                  <ul>
+                    <li className="d-flex align-items-center">
+                      <i>
+                        <RiCircleFill size={14} />
+                      </i>{" "}
+                      Available for Collaboration
+                    </li>
+                  </ul>
+                </div>
                 <div className="hero-btns">
-                    <a href="/Resume_of_Ariful Islam_Senior Visualizer.pdf" className="theme-btn" download>Download CV <i><RiDownloadLine size={16} /></i> </a>
+                  <a
+                    href="/Resume_of_Ariful Islam_Senior Visualizer.pdf"
+                    className="theme-btn"
+                    download
+                  >
+                    Download CV{" "}
+                    <i>
+                      <RiDownloadLine size={16} />
+                    </i>{" "}
+                  </a>
                 </div>
-                            </div>
-                        </SlideUp>
-                        <SlideUp>
-                            <PartnersMarquee />
-                        </SlideUp>
-                    </div>
-                    {/* <!-- / END ABOUT TEXT DESIGN AREA --> */}
-                </div>
-            </div>
-        </section>
-    )
-}
+              </div>
+            </SlideUp>
+            <SlideUp>
+              <PartnersMarquee />
+            </SlideUp>
+          </div>
+          {/* <!-- / END ABOUT TEXT DESIGN AREA --> */}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/sections/partnersMarquee.jsx
+++ src/components/sections/partnersMarquee.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React from "react";
 import Marquee from "react-fast-marquee";
 
 const PartnersMarquee = () => {
-    return (
-        <div className="about-content-part-bottom">
-            <h2>Company I Worked With</h2>
-            <div className="company-list">
-                <div className="scroller">
-                    <div className="scroller__inner">
-                        <Marquee>
-                            <img src={"/images/client-logos/partner1.png"} alt="" />
-                            <img src={"/images/client-logos/partner2.png"} alt="" />
-                            <img src={"/images/client-logos/partner3.png"} alt="" />
-                            <img src={"/images/client-logos/partner4.png"} alt="" />
-                            <img src={"/images/client-logos/partner5.png"} alt="" />
-                            <img src={"/images/client-logos/partner1.png"} alt="" />
-                        </Marquee>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="about-content-part-bottom">
+      <h2>Company I Worked With</h2>
+      <div className="company-list">
+        <div className="scroller">
+          <div className="scroller__inner">
+            <Marquee>
+              <img src={"/images/client-logos/partner-01.png"} alt="" />
+              <img src={"/images/client-logos/partner-02.png"} alt="" />
+              <img src={"/images/client-logos/partner-03.png"} alt="" />
+              <img src={"/images/client-logos/partner-04.png"} alt="" />
+              <img src={"/images/client-logos/partner-05.png"} alt="" />
+              <img src={"/images/client-logos/partner-06.png"} alt="" />
+              <img src={"/images/client-logos/partner-07.png"} alt="" />
+              <img src={"/images/client-logos/partner-08.png"} alt="" />
+              <img src={"/images/client-logos/partner-09.png"} alt="" />
+              <img src={"/images/client-logos/partner-10.png"} alt="" />
+            </Marquee>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default PartnersMarquee
+export default PartnersMarquee;
